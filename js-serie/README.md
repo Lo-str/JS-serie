d5ba0bf3dcd16ff247277b9395a35d46d053835f
LoS <lostreit@pm.me>
2025-11-07 17:20:41 +0100

    created bank-account.js and started project structure

--- js-serie/README.md
+++ js-serie/README.md
@@ -93,19 +93,12 @@ JS3: Conditional Statements & Functions
 
   Create an object called account that has the following properties:
 
-  accountName, should be the data type string
-  this property should contain the name of the account holder
-
+  - accountName, should be the data type string --> this property should contain the name of the account holder
   - balance, should be the data type number --> this property should contain the total amount of the account
-
   - getBalance, should be a function --> this function should display the total amount of the account to the user
-
   - deposit, also a function --> this function should be able to deposit money onto the balance of the account
-
   - withdrawal, also a function --> this function should be able do withdraw money from the balance of the account
-
   - getAccountName, function as well --> this function should display the account holders name to the user
-
   - accountError, same as above function! --> this one is a bit tricky... it's up to you to figure out how or what you should use this for.
 
   HINT: it's more a thinking poblem than a technical problem
--- js-serie/src/js/bank-account.js
+++ js-serie/src/js/bank-account.js
@@ -1,1 +1,59 @@
 //========== Bank Account ==========\\
+
+const account = {
+  accountName: "Lo", // name of the account holder
+  balance: 0, // total amount of the account
+  getBalance() {}, // display the total amount of the account to the user
+  deposit(amount) {
+    let amount = parseFloat(
+      prompt(`Please enter the amount you would like to deposit`)
+    );
+
+    let check = alert(`You would like to deposit $${amount}?`);
+    confirm(
+      `Your deposit was successfull! Your balance is now ${account.balance}`
+    );
+  }, // deposit money onto the balance of the account
+  withdrawal(amount) {}, // withdraw money from the balance of the account
+  getAccountName() {}, // display the account holders name to the user
+  accountError(message) {}, // display an error message to the user
+  exitAccount() {}, // exit the account
+};
+
+function atm() {
+  let menu = true;
+  let options = prompt(`Please choose an option:`);
+  while (menu) {
+    switch (options) {
+      case 1:
+        console.log(getAccountName());
+        break;
+
+      case 2:
+        console.log(getBalance());
+        break;
+
+      case 3:
+        console.log(deposit());
+        break;
+
+      case 4:
+        console.log(withdrawal());
+        break;
+
+      case 5:
+        console.log(exitAccount());
+        break;
+
+      default:
+        console.log(accountError());
+    }
+  }
+}
+
+// function welcome(firstName, lastName) {
+//   let fullName = firstName + lastName;
+//   confirm(`Welcome to your bank webplatform ${fullName}!`)
+//   return fullName;
+// }
+// let fullName = prompt(`Hi! Please enter your ${firstName} and ${lastName}.`);
